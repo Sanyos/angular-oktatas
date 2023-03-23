a9a7b7c7855c361919e441f2196c9d147973c2e1
Teleki Sándor <sanyos07@gmail.com>
2023-03-23 20:29:58 +0100

    slider service letrehozasa, hazi alap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageSliderComponent } from './shared/image-slider/image-slider.component';
+import { HighlightDirective } from './shared/highlight/highlight.directive';
+import { SelectButtonComponent } from './shared/select-button/select-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSliderComponent,
+    HighlightDirective,
+    SelectButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/image-slider/image-slider.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { SlideServiceService } from 'src/app/core/services/slide/slide-service.service';
+import { Slide } from 'src/app/core/types/slide/slide.type';
+
+@Component({
+  selector: 'app-image-slider',
+  templateUrl: './image-slider.component.html',
+  styleUrls: ['./image-slider.component.scss']
+})
+export class ImageSliderComponent {
+
+  @Output() slideSelected: EventEmitter<number> = new EventEmitter<number>();
+  @Input() triggerIndex: number = 0;
+
+  constructor(public readonly slideService: SlideServiceService){}
+
+
+  ngOnChanges(changes:SimpleChanges){
+    if(changes['triggerIndex'] && !changes['triggerIndex'].firstChange){
+      this.emitSlideIndex();
+    }
+  }
+
+  dotClicked(slideIndex:number):void{
+    this.slideService.setCurrentIndex(slideIndex);
+  }
+
+  emitSlideIndex(slideIndex?:number):void{
+    if(slideIndex){
+      this.slideSelected.emit(slideIndex);
+    }else{
+      this.slideSelected.emit(this.slideService.getCurrentIndex());
+    }
+  }
+
+  leftArrowClicked(){
+    this.slideService.turnToPreviousImage();
+  }
+
+  rightArrowClicked(){
+    this.slideService.turnToNextImage();
+  }
+
+}
+
+
