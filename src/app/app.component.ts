095ddc91ca0d18dbb226b5272e70ff3a4944f922
Teleki Sándor <sanyos07@gmail.com>
2023-03-30 17:57:04 +0200

    form kezeles bevezeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+
+  regForm: FormGroup;
+
+  constructor(private readonly httpClient: HttpClient){
+
+    this.regForm = new FormGroup({
+      'firstName': new FormControl("Teszt",[Validators.required]),
+      'lastName': new FormControl("Elek",[Validators.required,]),
+      'age':new FormControl(20,[Validators.required, ]),
+    });
+  }
+
+  register(){
+    if(this.regForm.status !== 'INVALID'){
+      console.log("Elküldöm az api kérést ezzel: ",this.regForm.value);
+      this.sendRegRequest(this.regForm.value).then((res)=>{
+
+
+      })
+      console.log("Ez a sor a promise után van!!!!");
+    }
+  }
+
+  private sendRegRequest(data:any):Promise<any>{
+    data.age += 1;
+     return this.httpClient.post("https://dummyjson.com/users/add",data).toPromise();
+  }
+
+
+
+
 }
